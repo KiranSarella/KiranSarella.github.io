6e1fa417618e516a91afe532be8fba1651040f98
Kiran Sarella <>
2024-08-15 13:09:11 +0530

    basic profile created

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,119 @@
-<h1>Welcome, I'm Kiran Sarella</h1>
+<script>
+
+    import './style.css'
+    
+</script>
+
+<h1>Kiran Sarella</h1>
+
+Senior Software Enginner, <div class="light">Hyderabad, India</div>
+<div></div>
+
+<div></div>
+<br>
+<b>11 years</b> of experience
+<div class="dashlist">
+    <ul>
+        <li>
+            iOS & mac app development <i>(8 years)</i>
+        </li>
+        <li>
+            Node.js <i>(3 years)</i>
+        </li>
+    </ul>
+</div>
+
+<h2>
+    Skills
+</h2>
+       
+<div class="tabs">
+    <b>iOS, macOS</b>
+    <ul>
+        <li>Swift</li>
+        <li>SwiftUI</li>
+        <li>Concurrency</li>
+        <li>Combine</li>
+        <li>Swift Data</li>
+        <li>TextKit</li>
+        <li>Accessibility</li>
+        <li>CloudKit</li>
+        <li>StoreKit</li>
+        <li>APNS</li>
+        <li>App Store Connect</li>
+    </ul>
+
+    <b>Node.js</b>
+    <ul>
+        <li>JavaScript</li>
+        <li>Node.js</li>
+        <li>Express.js</li>
+        <li>MySql</li>
+        <li>Knex.js</li>
+        <li>OAuth 2</li>
+        <li>OpenID Connect</li>
+        <li>JWT</li>
+        <li>RBAC</li>
+        <li>Cron Jobs</li>
+        <li>AWS lambda functions</li>
+        <li>Redis Cache</li>
+    </ul>
+    <b>Architecture</b>
+    <ul>
+        <li>MVC</li> 
+        <li>MVVM</li>
+        <li>Domain Driven Design</li>
+        <li>Design Patterns</li>
+        <li>Unit Testing</li>
+        <li>Test Doubles</li>
+    </ul>
+    <b>Core</b>
+    <ul>
+        <li>Data Structures</li> 
+        <li>Algorithms</li>
+    </ul>
+    <b>Design</b>
+    <ul>
+        <li>Figma</li>
+        <li>UI/UX</li>
+        <li>Freeform</li>
+    </ul>
+    <b>Others</b>
+    <ul>
+        <li>Vapor Server</li> 
+        <li>SvelteKit</li>
+        <li>WebSocket</li>
+        <li>WebRTC</li>
+        <li>Figma</li>
+        <li>UI/UX</li>
+        <li>Git</li>
+        <li>Jira</li>
+    </ul>
+</div>
+  
+
+<h2>
+    Links
+</h2>
+
+<div>
+    <a href="https://kiran-sarella.medium.com/list/technical-articles-1dd360fa0b98" class="link" target="_blank">
+        Technical Articles
+    </a>
+    <a href="https://github.com/KiranSarella" class="link" target="_blank">
+        Github
+    </a>
+    <a href="https://www.linkedin.com/in/KiranSarella/" class="link" target="_blank">
+        LinkedIn
+    </a>    
+</div>
+
+
+<h2>
+    Contact
+</h2>
+
+<div>
+    <a href="mailto:kiran.sarella@gmail.com" class="link">kiran.sarella@gmail.com</a>
+</div>
 
